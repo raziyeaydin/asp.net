b5fb401f2856151fbfae7ed44c131434d5b93c80
Raziye AYDIN <raziye.aydin@bil.omu.edu.tr>
2014-07-21 23:30:53 +0300

    DB_islemleri

---
+++ bd_islemleri/bd_islemleri/default.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="bd_islemleri._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        isim
+    <asp:TextBox ID="txtBox1" runat="server" /> &nbsp;
+        id
+        <asp:TextBox ID="txtBox2" runat="server"></asp:TextBox>
+        <br />
+    <asp:Button ID="listeleBtn" Text="Listele" runat="server" Width="63px" OnClick="listeleBtn_Click" />
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:Button ID="ekleBtn" Text="Ekle" runat="server" style="margin-left: 0px" Width="55px" OnClick="ekleBtn_Click" />
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:Button ID="silBtn" Text="Sil" runat="server" style="margin-left: 0px" Width="64px" OnClick="silBtn_Click" />
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:Button ID="guncelleBtn" Text="Güncelle" runat="server" style="margin-left: 0px" OnClick="guncelleBtn_Click" Visible="False" />
+
+        <br />
+        <br />
+
+     &nbsp;&nbsp;&nbsp;&nbsp;<asp:DataGrid ID="dataGrid1" runat="server"  >
+            
+        </asp:DataGrid>
+&nbsp;<br />
+&nbsp;&nbsp;&nbsp;&nbsp;
+   
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ bd_islemleri/bd_islemleri/default.aspx.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.OleDb;
+using System.Data;
+
+namespace bd_islemleri
+{
+    public partial class _default : System.Web.UI.Page
+    {
+            
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+     
+        protected void listeleBtn_Click(object sender, EventArgs e)
+        {
+            OleDbConnection baglanti = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + Server.MapPath("App_Data/dbc.mdb") + ";Persist Security Info=True");
+
+            String listele = "select * from [isimT]";
+            baglanti.Open();
+
+            OleDbDataAdapter da = new OleDbDataAdapter(listele, baglanti);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGrid1.DataSource = ds.Tables[0];
+            dataGrid1.DataBind();
+            baglanti.Close();
+        }
+
+        protected void ekleBtn_Click(object sender, EventArgs e)
+        {
+            try{
+
+            OleDbConnection baglanti = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + Server.MapPath("App_Data/dbc.mdb") + ";Persist Security Info=True");
+            baglanti.Open();
+
+            OleDbCommand ekle = new OleDbCommand("insert into isimT(isim) values(@isim)", baglanti);
+           
+            ekle.Parameters.AddWithValue("@isim", txtBox1.Text);
+            ekle.ExecuteNonQuery();
+            baglanti.Close();
+            dataGrid1.DataBind();
+            Response.Write("veriniz eklendi");
+            }
+
+            catch(OleDbException){
+                Response.Write("çalışmadı");
+            }
+            
+            
+
+        }
+
+        protected void silBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OleDbConnection baglanti = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + Server.MapPath("App_Data/dbc.mdb") + ";Persist Security Info=True");
+                baglanti.Open();
+                OleDbCommand sil= new OleDbCommand("delete from isimT where isim=@isim", baglanti);
+
+                sil.Parameters.AddWithValue("@isim", txtBox1.Text);
+                sil.ExecuteNonQuery();
+                baglanti.Close();
+                dataGrid1.DataBind();
+                Response.Write("veriniz silindi");
+
+
+            }
+            catch(OleDbException){
+                Response.Write("veriniz silinemedi.");
+            }
+
+        }
+
+        protected void guncelleBtn_Click(object sender, EventArgs e)
+        {
+            /*
+            try
+            {
+                string isim = txtBox1.Text.ToString();
+                int id = Convert.ToInt32(txtBox2.Text.ToString());
+                OleDbConnection baglanti = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + Server.MapPath("App_Data/dbc.mdb") + ";Persist Security Info=True");
+                baglanti.Open();
+                OleDbCommand guncelle = new OleDbCommand("UPDATE isimT SET isim = @isim WHERE id = @id;", baglanti);
+               
+                guncelle.Parameters.AddWithValue("@isim",isim );
+                guncelle.Parameters.AddWithValue("@id",id );
+                int durum = guncelle.ExecuteNonQuery();
+                baglanti.Close();
+                dataGrid1.DataBind();
+                Response.Write(durum);
+                Response.Write("veriniz güncellendi");
+    
+
+
+            }
+            catch (OleDbException)
+            {
+                Response.Write("veriniz güncellenemedi");
+            }
+             */
+        }
+       
+    }
+}
